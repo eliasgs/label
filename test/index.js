ced6c9993d60c818e829111c6d011a0361f5e711
Elias G. Schneevoigt <eliasgs@gmail.com>
2013-12-19 21:25:17 +0100

    Fix tests

--- test/index.js
+++ test/index.js
@@ -10,24 +10,24 @@ describe('label', function () {
       assert.ok(words[0].length > 1);
       assert.ok(words[1].length > 1);
     });
-    it('should default to a capitalized sentence', function () {
+    it('should default to a uncapitalized sentence', function () {
       var test = label.generate();
-      var words = test.split(' ');
-      var char1 = words[0][0];
-      var char2 = words[1][0];
-      assert.equal(char1.toUpperCase(), char1);
-      assert.equal(char2.toUpperCase(), char2);
-    });
-    it('should generate a sentence that is not capitalized', function () {
-      var test = label.generate(true);
       var words = test.split(' ');
       var char1 = words[0][0];
       var char2 = words[1][0];
       assert.equal(char1.toLowerCase(), char1);
       assert.equal(char2.toLowerCase(), char2);
     });
+    it('should be able to capitalize', function () {
+      var test = label.generate(true);
+      var words = test.split(' ');
+      var char1 = words[0][0];
+      var char2 = words[1][0];
+      assert.equal(char1.toUpperCase(), char1);
+      assert.equal(char2.toUpperCase(), char2);
+    });
     it('should be posible to change delimiter', function () {
-      var test = label.generate(true, '_');
+      var test = label.generate(false, '_');
       // we also accept words like t-shirt
       assert(/^[a-z-]+_[a-z-]+$/.test(test));
     });
